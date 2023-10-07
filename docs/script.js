e5dc05418f925c8d0b66194700a519792e14bc5d
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2023-10-07 20:54:52 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,20 @@
 new HTMLDocument(body => {
-	body.write('hi');
+	block();
+	const box = body.append('div', ['style*']);
+	new Style({
+		flex: 'auto',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		overflow: 'hidden',
+	}).apply(box);
+	const board = box.append('div', ['style*']);
+	new Style({
+		display: 'grid',
+		grid: 'repeat(8, 11%) / repeat(8, 11%)',
+		placeItems: 'stretch',
+		placeContent: 'space-evenly',
+	}).apply(board);
 }).load();
 
 function block() {
