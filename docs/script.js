23ac88cef9989ac60dbcdfb3082a0dc7693ace7f
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2023-10-09 12:30:29 +0200

    #

--- docs/script.js
+++ docs/script.js
@@ -1,53 +1,65 @@
 new HTMLDocument(body => {
 	block();
-	const box = body.append('div', ['style*']);
-	new Style({
-		flex: 'auto',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		overflow: 'hidden',
-	}).apply(box);
-	const board = box.append('div', ['style*']);
-	new Style({
-		display: 'grid',
-		grid: 'repeat(8, 11%) / repeat(8, 11%)',
-		placeItems: 'stretch',
-		placeContent: 'space-evenly',
-	}).apply(board);
-	const squares = [];
-	for (let [i, j] = [0, 0]; i < 8; (() => {
-		j++;
-		if (j === 8) {
-			j = 0;
-			i++;
-		}
-	})()) {
-		squares[i] ? () => false : (() => {
-			squares[i] = [];
-		})();
-		squares[i][j] = board.append('div', ['style*']);
-		squares[i][j].value = -1;
-		squares[i][j].set = function (value) {
-			this.value = value;
-			this.clear().write(fonts[value]);
-		};
+	prepare();
+	start();
+	function prepare() {
+		const box = body.append('div', ['style*']);
 		new Style({
-			gridArea: (i + 1) + '/' + (j + 1),
-			background: document.palette.light,
-			cursor: 'default',
-			textAlign: 'center',
-		}).apply(squares[i][j]);
+			flex: 'auto',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			overflow: 'hidden',
+		}).apply(box);
+		const board = box.append('div', ['style*']);
+		new Style({
+			display: 'grid',
+			grid: 'repeat(8, 11%) / repeat(8, 11%)',
+			placeItems: 'stretch',
+			placeContent: 'space-evenly',
+			background: '#182f',
+		}).apply(board);
+		const squares = [];
+		for (let [i, j] = [0, 0]; i < 8; (() => {
+			j++;
+			if (j === 8) {
+				j = 0;
+				i++;
+			}
+		})()) {
+			squares[i] ? () => false : (() => {
+				squares[i] = [];
+			})();
+			squares[i][j] = board.append('div', ['style*']);
+			squares[i][j].value = -1;
+			squares[i][j].set = function (value) {
+				this.value = value;
+				this.clear().write(fonts[value]);
+			};
+			new Style({
+				gridArea: (i + 1) + '/' + (j + 1),
+				background: document.palette.light,
+				cursor: 'default',
+				textAlign: 'center',
+			}).apply(squares[i][j]);
+		}
+		const fonts = {
+			'-1': '',
+			0: '\u26ab',
+			1: '\u26aa',
+		};
 	}
-	const fonts = {
-		'-1': '',
-		0: '\u26ab',
-		1: '\u26aa',
+	function start() {
+	
 	}
+	function next() {}
+	function pass() {}
+	function end() {}
+	
 }).load();
 
-function block() {
-	document.body.oncontextmenu = () => false;
-	document.body.onselectstart = () => false;
-	document.body.ondragstart = () => false;
+function block(bool) {
+	document.body.oncontextmenu = () => !bool;
+	document.body.onselectstart = () => !bool;
+	document.body.ondragstart = () => !bool;
 }
